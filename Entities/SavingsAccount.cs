db3698bd36111d1785ec76e2e29a321aca63a752
Matheusllb <matheusluislopesbatista@gmail.com>
2023-05-21 22:07:06 -0300

    Criada a classe Conta poupança

---
+++ Entities/SavingsAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace InheritanceAccount.Entities
+{
+    public class SavingsAccount : Account
+    {
+        public double InterestRate { get; protected set; }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate; 
+        }
+    }
+}
